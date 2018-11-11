6424470d35eff8d0502f3125300bcca597609a0b
Aktoty Rysdaulet <35283838+Aktoty00@users.noreply.github.com>
2018-11-11 18:30:47 +0600

    Add files via upload

---
+++ lab3/src/task_3/Employee.java
@@ -0,0 +1,44 @@
+package task_3;
+
+public class Employee extends Person{
+    private double salary;
+    private int year;
+    private String insuranceNumber;
+    public Employee(){
+        super();
+        this.salary = 0;
+        this.year = 0;
+        this.insuranceNumber = "no_insuranceNumber";
+    }
+    public Employee(String initialName, double initialSalary, int initialYear, String initialInsuranceNumber){
+        super(initialName);
+        salary = initialSalary;
+        year = initialYear;
+        insuranceNumber = initialInsuranceNumber;
+    }
+    public double getSalary(){
+        return this.salary;
+    }
+
+    public String getInsuranceNumber(){
+        return this.insuranceNumber;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Employee)
+            return super.equals(obj)&&
+                    this.insuranceNumber == ((Employee) obj).insuranceNumber&&
+                    this.salary == ((Employee) obj).salary&&
+                    this.year == ((Employee) obj).year;
+        else
+            return false;
+    }
+    @Override
+    public String toString(){
+        return(super.toString() + "; Employee's salary is:  " + this.salary + "; Employee's year is:  " + this.year + "; Employee's insurance number is:  " +this.insuranceNumber + "\n");
+    }
+}
